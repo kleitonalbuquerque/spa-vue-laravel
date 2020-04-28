adc8f34291d51e554abf86112a55121bcf2a76bc
kleitonalbuquerque <kleiton2102@gmail.com>
2020-04-28 14:44:24 -0300

    Configuraçao do projeto com Materialize

---
+++ social/src/pages/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="">
+      <h1>Social</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
